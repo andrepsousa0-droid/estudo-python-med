4214059d934bdda355d79ef7cc3b3a7d7906dffd
André Sousa <andrepsousa0@gmail.com>
2026-02-19 12:58:27 -0100

    feats: criar função de analisar pacientes

--- prescricao.py
+++ prescricao.py
@@ -1,10 +1,10 @@
-minha_prescricao=['Acido Acetilsalicilico', 'Alcool', 'Insulinas']  #listas
-minha_prescricao.append('Anticoagulantes Orais')
+#minha_prescricao=['Acido Acetilsalicilico', 'Alcool', 'Insulinas']  #listas
+#minha_prescricao.append('Anticoagulantes Orais')
 
-Acido_Acetilsalicilico = {'Alcool': 'potencialmente grave'}    #dicionario
+#Acido_Acetilsalicilico = {'Alcool': 'potencialmente grave'}    #dicionario
 #experimentando gavetasprint(Acido_Acetilsalicilico['alcool'])
 
-Acido_Acetilsalicilico ['Alcool'] = 'muito grave'
+#Acido_Acetilsalicilico ['Alcool'] = 'muito grave'
 # serviu para ver a alteração de grave para muito grave print(Acido_Acetilsalicilico['alcool'])
 
 #for Acido_Acetilsalicilico in minha_prescricao:
@@ -13,11 +13,33 @@ Acido_Acetilsalicilico ['Alcool'] = 'muito grave'
 #for indice, medicamento in enumerate (minha_prescricao, start=1):
  #   print(indice, medicamento) #serve para enumerar a lista
  
-avisos= {'Alcool' : 'Risco de hemorragia se misturado com insulina'}
-
-for indice, med in enumerate (minha_prescricao, start=1):
-    print(f"{indice}.{med}")
+#avisos= {'Alcool' : 'Risco de hemorragia se misturado com insulina'}
+#def verificar_receita(lista_meds):
+       
+#    for indice, med in enumerate (minha_prescricao, start=1):
+ #       print(f"{indice}.{med}")
 
 #Se o medicamento "med" estiver no dicionário de avisos
-    if med in avisos:
-        print(f"\t ATENÇÃO :{avisos[med]}")
+  #      if med in avisos:
+            #print(f"\t ATENÇÃO :{avisos[med]}")
+
+
+
+#1 A base de Dados
+avisos = {'Alcool': 'Pode causar hemorragia se misturado com insulina'}
+
+#2 Definir função
+def analisar_paciente(lista_meds):
+        print('\n--- A analisar prescrição ---')
+        for indice, meds in enumerate (lista_meds, start=1):
+            print(f'{indice}.{meds}')
+            if meds in avisos:
+                print(f'\t ATENÇÃO: {avisos[meds]}')
+
+
+#3 Os dados
+minha_prescricao = ['Acido Acetilsalicilico', 'Anticoagulantes Orais', 'Alcool', 'Insulina']
+
+#4 Analisar o paciente
+
+analisar_paciente(minha_prescricao)
